35b25ca004c6809ce89af7baecfc5cc19ddf61d3
khanhlqhe150122@fpt.edu.vn <khanhlqhe150122@fpt.edu.vn>
2023-06-08 00:57:46 +0700

    Add sign up screen, change language to Vietnamese, add navigation

--- App.tsx
+++ App.tsx
@@ -1,9 +1,32 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
+import { View } from "react-native";
 import React from "react";
+import SignIn from "./src/pages/auth/SignIn";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import SignUp from "./src/pages/auth/SignUp";
 
+const Stack = createNativeStackNavigator();
+
 const App = () => {
-    return <SignUp />;
+  return (
+    <View className="flex-1">
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="SignIn" component={SignIn} />
+          <Stack.Screen
+            name="SignUp"
+            component={SignUp}
+            options={{ title: "SignUp" }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </View>
+  );
 };
 
 export default App;
--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -1,44 +1,79 @@
 import React, { useState, type ReactElement } from "react";
-import { Text, TextInput, View } from "react-native";
+import { Text, TextInput, View, TouchableOpacity } from "react-native";
 import { authInputStyles } from "components/auth/AuthInput";
 import AuthButton from "components/auth/AuthButton";
 
-const SignUp = (): ReactElement<void> => {
+// TODO: determine the type of navigation
+interface SignUpProps {
+  navigation: any;
+}
+
+const SignUp = (props: SignUpProps): ReactElement<void> => {
+  const { navigation } = props;
   const [accountId, setAccountId] = useState<string>("");
   const [password, setPassword] = useState<string>("");
 
-  const handleSignIn = (): void => {
+  const handleSignUp = (): void => {
     console.log("Sign In");
+  };
+  const handleSignInRedirect = (): void => {
+    navigation.navigate("SignIn");
   };
 
   return (
     <View className="flex-1 my-16 mx-8">
       <View>
-        <Text className="text-4xl font-bold">Hello,</Text>
-        <Text className="text-2xl">Welcome back,</Text>
+        <Text className="text-2xl font-bold">Tạo tài khoản mới</Text>
       </View>
-      <View className="mt-16 w-full">
-        <Text>Email</Text>
+      <View className="mt-8 w-full">
+        <Text>Tài khoản email</Text>
         <TextInput
           className={authInputStyles.textInput}
           onChangeText={setAccountId}
           defaultValue={accountId}
-          placeholder="Enter your username or email"
+          placeholder="Nhập tài khoản email của bạn"
         />
-        <Text className="mt-8">Enter Password</Text>
+        <Text className="mt-8">Tên đăng nhập</Text>
+        <TextInput
+          className={authInputStyles.textInput}
+          onChangeText={setPassword}
+          defaultValue={password}
+          textContentType="none"
+          placeholder="Nhập tên đăng nhập"
+        />
+        <Text className="mt-8">Tên</Text>
+        <TextInput
+          className={authInputStyles.textInput}
+          onChangeText={setPassword}
+          defaultValue={password}
+          textContentType="none"
+          placeholder="Nhập tên của bạn"
+        />
+        <Text className="mt-8">Mật khẩu</Text>
         <TextInput
           className={authInputStyles.textInput}
           onChangeText={setPassword}
           defaultValue={password}
           textContentType="password"
-          placeholder="Enter your password"
+          placeholder="Nhập mật khẩu của bạn"
+        />
+        <Text className="mt-8">Nhập lại mật khẩu</Text>
+        <TextInput
+          className={authInputStyles.textInput}
+          onChangeText={setPassword}
+          defaultValue={password}
+          textContentType="password"
+          placeholder="Nhập lại mật khẩu của bạn"
         />
       </View>
-      <View className="mt-4">
-        <Text style={{ color: "#FFCD80" }}>Forgot Password?</Text>
+      <View className="mt-8">
+        <AuthButton title="Đăng ký" onPress={handleSignUp} />
       </View>
-      <View className="mt-4">
-        <AuthButton title="Sign In" onPress={handleSignIn} />
+      <View className="mt-4 flex-row justify-center">
+        <Text>Bạn đã có tài khoản? </Text>
+        <TouchableOpacity onPress={handleSignInRedirect}>
+          <Text>Đăng nhập</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
