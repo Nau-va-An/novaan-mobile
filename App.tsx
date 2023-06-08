22898756595bfc04f8777e5cd2d9f1894cc44b34
Hoang Tien Minh <htminh.1001@gmail.com>
2023-06-08 23:48:39 +0700

    fix: add prettierrc to format code with consistency

--- App.tsx
+++ App.tsx
@@ -1,38 +1,38 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
-import { View } from 'react-native';
-import React from 'react';
-import SignIn from './src/pages/auth/SignIn';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import SignUp from './src/pages/auth/SignUp';
+import { View } from 'react-native'
+import React from 'react'
+import SignIn from './src/pages/auth/SignIn'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import SignUp from './src/pages/auth/SignUp'
 
 // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
 export type RootStackParamList = {
-  SignIn: undefined;
-  SignUp: undefined;
-};
+    SignIn: undefined
+    SignUp: undefined
+}
 
-const RootStack = createNativeStackNavigator<RootStackParamList>();
+const RootStack = createNativeStackNavigator<RootStackParamList>()
 
 const App = () => {
-  return (
-    <View className="flex-1">
-      <NavigationContainer>
-        <RootStack.Navigator
-          screenOptions={{
-            headerShown: false,
-          }}
-        >
-          <RootStack.Screen name="SignIn" component={SignIn} />
-          <RootStack.Screen
-            name="SignUp"
-            component={SignUp}
-            options={{ title: 'SignUp' }}
-          />
-        </RootStack.Navigator>
-      </NavigationContainer>
-    </View>
-  );
-};
+    return (
+        <View className="flex-1">
+            <NavigationContainer>
+                <RootStack.Navigator
+                    screenOptions={{
+                        headerShown: false,
+                    }}
+                >
+                    <RootStack.Screen name="SignIn" component={SignIn} />
+                    <RootStack.Screen
+                        name="SignUp"
+                        component={SignUp}
+                        options={{ title: 'SignUp' }}
+                    />
+                </RootStack.Navigator>
+            </NavigationContainer>
+        </View>
+    )
+}
 
-export default App;
+export default App
--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -7,80 +7,80 @@ import AuthButton from "components/auth/AuthButton";
 import { type RootStackParamList } from "App";
 
 interface SignUpProps {
-  navigation: NativeStackNavigationProp<RootStackParamList, "SignIn">;
+    navigation: NativeStackNavigationProp<RootStackParamList, "SignIn">;
 }
 
-const SignUp = (props: SignUpProps): ReactElement<void> => {
-  const { navigation } = props;
+const SignUp = (props: SignUpProps): ReactElement<SignUpProps> => {
+    const { navigation } = props;
 
-  const [accountId, setAccountId] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
+    const [accountId, setAccountId] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
 
-  const handleSignUp = (): void => {
-    console.log("Sign In");
-  };
+    const handleSignUp = (): void => {
+        console.log("Sign In");
+    };
 
-  const handleSignInRedirect = (): void => {
-    navigation.navigate("SignIn");
-  };
+    const handleSignInRedirect = (): void => {
+        navigation.navigate("SignIn");
+    };
 
-  return (
-    <View className="flex-1 my-16 mx-8">
-      <View>
-        <Text className="text-2xl font-bold">Tạo tài khoản mới</Text>
-      </View>
-      <View className="mt-8 w-full">
-        <Text>Tài khoản email</Text>
-        <TextInput
-          className={authInputStyles.textInput}
-          onChangeText={setAccountId}
-          defaultValue={accountId}
-          placeholder="Nhập tài khoản email của bạn"
-        />
-        <Text className="mt-8">Tên đăng nhập</Text>
-        <TextInput
-          className={authInputStyles.textInput}
-          onChangeText={setPassword}
-          defaultValue={password}
-          textContentType="none"
-          placeholder="Nhập tên đăng nhập"
-        />
-        <Text className="mt-8">Tên</Text>
-        <TextInput
-          className={authInputStyles.textInput}
-          onChangeText={setPassword}
-          defaultValue={password}
-          textContentType="none"
-          placeholder="Nhập tên của bạn"
-        />
-        <Text className="mt-8">Mật khẩu</Text>
-        <TextInput
-          className={authInputStyles.textInput}
-          onChangeText={setPassword}
-          defaultValue={password}
-          textContentType="password"
-          placeholder="Nhập mật khẩu của bạn"
-        />
-        <Text className="mt-8">Nhập lại mật khẩu</Text>
-        <TextInput
-          className={authInputStyles.textInput}
-          onChangeText={setPassword}
-          defaultValue={password}
-          textContentType="password"
-          placeholder="Nhập lại mật khẩu của bạn"
-        />
-      </View>
-      <View className="mt-8">
-        <AuthButton title="Đăng ký" onPress={handleSignUp} />
-      </View>
-      <View className="mt-4 flex-row justify-center">
-        <Text>Bạn đã có tài khoản? </Text>
-        <TouchableOpacity onPress={handleSignInRedirect}>
-          <Text>Đăng nhập</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
+    return (
+        <View className="flex-1 my-16 mx-8">
+            <View>
+                <Text className="text-2xl font-bold">Tạo tài khoản mới</Text>
+            </View>
+            <View className="mt-8 w-full">
+                <Text>Tài khoản email</Text>
+                <TextInput
+                    className={authInputStyles.textInput}
+                    onChangeText={setAccountId}
+                    defaultValue={accountId}
+                    placeholder="Nhập tài khoản email của bạn"
+                />
+                <Text className="mt-8">Tên đăng nhập</Text>
+                <TextInput
+                    className={authInputStyles.textInput}
+                    onChangeText={setPassword}
+                    defaultValue={password}
+                    textContentType="none"
+                    placeholder="Nhập tên đăng nhập"
+                />
+                <Text className="mt-8">Tên</Text>
+                <TextInput
+                    className={authInputStyles.textInput}
+                    onChangeText={setPassword}
+                    defaultValue={password}
+                    textContentType="none"
+                    placeholder="Nhập tên của bạn"
+                />
+                <Text className="mt-8">Mật khẩu</Text>
+                <TextInput
+                    className={authInputStyles.textInput}
+                    onChangeText={setPassword}
+                    defaultValue={password}
+                    textContentType="password"
+                    placeholder="Nhập mật khẩu của bạn"
+                />
+                <Text className="mt-8">Nhập lại mật khẩu</Text>
+                <TextInput
+                    className={authInputStyles.textInput}
+                    onChangeText={setPassword}
+                    defaultValue={password}
+                    textContentType="password"
+                    placeholder="Nhập lại mật khẩu của bạn"
+                />
+            </View>
+            <View className="mt-8">
+                <AuthButton title="Đăng ký" onPress={handleSignUp} />
+            </View>
+            <View className="mt-4 flex-row justify-center">
+                <Text>Bạn đã có tài khoản? </Text>
+                <TouchableOpacity onPress={handleSignInRedirect}>
+                    <Text>Đăng nhập</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
 };
 
 export default SignUp;
