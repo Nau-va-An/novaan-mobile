aa1a2595e23ab232f24472c2f84521d900473a82
Hoang Tien Minh <htminh.1001@gmail.com>
2023-06-15 23:16:45 +0700

    fix: update string values for sign in screen

--- src/common/strings.ts
+++ src/common/strings.ts
@@ -15,8 +15,8 @@ export const AUTH_USERNAME_TOO_SHORT =
 
 // Sign in screen
 export const SIGN_IN_GREETING = "Xin chào";
-export const SIGN_IN_EMAIL_TITLE = "Tên đăng nhập/Email";
-export const SIGN_IN_EMAIL_PLACEHOLDER = "Nhập tên đăng nhập hoặc email";
+export const SIGN_IN_EMAIL_TITLE = "Email";
+export const SIGN_IN_EMAIL_PLACEHOLDER = "Nhập email";
 export const SIGN_IN_PASSWORD_TITLE = "Mật khẩu";
 export const SIGN_IN_PASSWORD_PLACEHOLDER = "Nhập mật khẩu";
 export const SIGN_IN_SIGN_IN_BUTTON_TITLE = "Đăng nhập";
