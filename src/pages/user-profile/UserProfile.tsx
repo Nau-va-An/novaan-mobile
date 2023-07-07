c2b0130945ae262babe35b29de190ba96248ba28
Hoang Tien Minh <htminh.1001@gmail.com>
2023-07-07 22:16:16 +0700

    feat: user profile basic layout

--- src/pages/user-profile/UserProfile.tsx
+++ src/pages/user-profile/UserProfile.tsx
@@ -1,29 +1,40 @@
-import React, { useEffect } from "react";
-import { View } from "react-native";
+import React, { FC, type ReactElement, useEffect, useState } from "react";
+import { Text, View } from "react-native";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import CreatedPosts from "./CreatedPosts";
 import SavedPosts from "./SavedPosts";
 import { useProfileInfo } from "@/api/profile/ProfileApi";
+import OverlayLoading from "@/common/components/OverlayLoading";
+import { type MaterialBottomTabNavigationProp } from "@react-navigation/material-bottom-tabs";
+import { type BottomTabParamList } from "../MainScreens";
+import { Toast } from "react-native-toast-message/lib/src/Toast";
+import { Avatar } from "react-native-paper";
+import { customColors } from "@root/tailwind.config";
 
 // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
 export type UserProfileTabParamList = {
-    SavedPosts: undefined;
     CreatedPosts: undefined;
+    SavedPosts: undefined;
+    Following: undefined;
 };
 
 const Tab = createMaterialTopTabNavigator<UserProfileTabParamList>();
 
-const UserProfile: React.FC = () => {
+interface UserProfileProps {
+    navigation: MaterialBottomTabNavigationProp<BottomTabParamList>;
+}
+
+const UserProfile = (
+    props: UserProfileProps
+): ReactElement<UserProfileProps> => {
+    const { navigation } = props;
+
+    const [loading, setLoading] = useState(true);
+
     const { profileInfo, fetchPersonalProfile } = useProfileInfo();
 
     useEffect(() => {
-        fetchPersonalProfile()
-            .then((result) => {
-                !result && alert("Fetch profile failed");
-            })
-            .catch(() => {
-                alert("Fetch profile failed");
-            });
+        void handleFetchProfile();
     }, []);
 
     useEffect(() => {
@@ -34,25 +45,79 @@ const UserProfile: React.FC = () => {
         console.log(profileInfo);
     }, [profileInfo]);
 
+    const handleFetchProfile = async (): Promise<void> => {
+        setLoading(true);
+        try {
+            const fetched = await fetchPersonalProfile();
+            if (!fetched) {
+                throw new Error();
+            }
+        } catch {
+            navigation.navigate("Home");
+            Toast.show({
+                type: "error",
+                text1: "Failed to fetch user profile",
+            });
+        } finally {
+            setLoading(false);
+        }
+    };
+
     return (
-        <View className="flex">
-            <View
-                className="basis-2/5"
-                style={{ backgroundColor: "#fff" }}
-            ></View>
+        <View className="flex-1">
+            <View style={{ backgroundColor: "#fff" }}>
+                <View className="items-center justify-center mt-4">
+                    <Text className="text-cprimary-300 text-xl font-semibold">
+                        Trang đại diện
+                    </Text>
+                </View>
+                <View className="mx-6 mt-4 flex-row items-center justify-center">
+                    <Avatar.Text
+                        size={90}
+                        style={{
+                            backgroundColor: customColors.cprimary["300"],
+                        }}
+                        label="TE"
+                        className="mr-4"
+                    />
+                    <View className="flex-row flex-1">
+                        <View className="flex-1 items-center">
+                            <Text>Recipe</Text>
+                            <Text className="text-xl font-semibold text-cprimary-300">
+                                100
+                            </Text>
+                        </View>
+                        <View className="flex-1 items-center">
+                            <Text>Followers</Text>
+                            <Text className="text-xl font-semibold text-cprimary-300">
+                                2.5M
+                            </Text>
+                        </View>
+                        <View className="flex-1 items-center">
+                            <Text>Following</Text>
+                            <Text className="text-xl font-semibold text-cprimary-300">
+                                30
+                            </Text>
+                        </View>
+                    </View>
+                </View>
+                <View className="mx-8 my-4">
+                    <Text className="text-xl text-cprimary-300">Your Name</Text>
+                    <Text className="text-gray-600 italic mt-2">
+                        Oops, this user have not written much about themselves!
+                    </Text>
+                </View>
+            </View>
             <Tab.Navigator
-                className="basis-3/4"
+                className="flex-1"
                 screenOptions={{
-                    tabBarItemStyle: { width: 100 },
+                    tabBarItemStyle: {
+                        flex: 1,
+                        justifyContent: "center",
+                        alignItems: "center",
+                    },
                 }}
             >
-                <Tab.Screen
-                    name="SavedPosts"
-                    component={SavedPosts}
-                    options={{
-                        tabBarLabel: "Lưu trữ",
-                    }}
-                />
                 <Tab.Screen
                     name="CreatedPosts"
                     component={CreatedPosts}
@@ -60,7 +125,22 @@ const UserProfile: React.FC = () => {
                         tabBarLabel: "Bài đăng",
                     }}
                 />
+                <Tab.Screen
+                    name="SavedPosts"
+                    component={SavedPosts}
+                    options={{
+                        tabBarLabel: "Xem sau",
+                    }}
+                />
+                <Tab.Screen
+                    name="Following"
+                    component={CreatedPosts}
+                    options={{
+                        tabBarLabel: "Theo dõi",
+                    }}
+                />
             </Tab.Navigator>
+            {loading && <OverlayLoading />}
         </View>
     );
 };
