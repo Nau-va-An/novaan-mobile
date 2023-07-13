9b5d05d6dd9ac290517d441556d8cc9875e4546c
Hoang Tien Minh <htminh.1001@gmail.com>
2023-07-13 19:01:03 +0700

    refactor: refactor profile API posted content hook

--- src/api/profile/types/post.type.ts
+++ src/api/profile/types/post.type.ts
@@ -5,7 +5,6 @@ import {
 
 interface GetUserContentReturn {
     getNext: () => Promise<boolean>;
-    refresh: () => void;
     ended: boolean;
 }
 
@@ -15,4 +14,10 @@ export interface GetUserRecipeReturn extends GetUserContentReturn {
 
 export interface GetUserTipReturn extends GetUserContentReturn {
     tips: TipResponse[];
+}
+
+export interface GetUserContentHookReturn {
+    ended: boolean;
+    getContent: <T>(start: number, limit: number) => Promise<T[]>;
+    getNext: <T>() => Promise<T[]>;
 }
